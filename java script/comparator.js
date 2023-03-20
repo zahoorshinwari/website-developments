36f67b361a7a809adfb116709f9aacff95204cdc
zahoorkhan <khanzshinwari5371@gmail.com>
2023-03-20 23:53:29 +0500

    updated

--- java script/comparator.js
+++ java script/comparator.js
@@ -17,3 +17,30 @@ else
 {
     console.log("no");
 }
+
+
+
+
+
+
+
+
+//////////// combining comparator ////////////////
+
+// prompt("what is your name?");
+// prompt("who is your crush?");
+
+// var loveScore = Math.random() * 100 ;
+// loveScore = Math.floor(loveScore) + 1;
+// if(loveScore > 70){
+//     alert("your love score is " + loveScore + "%" + ", you love more with each other.");    
+// }
+// if(loveScore <= 70 && loveScore >= 30)
+// {
+//     alert("your love score is " + loveScore + "%");
+// }
+
+// if(loveScore < 30)
+// {
+//     alert("your love score is " + loveScore + "%" + " They will breakup!");
+// }
