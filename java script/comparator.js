eb578563cec157a8e7c9673613cbd6458b5d6664
zahoorkhan <khanzshinwari5371@gmail.com>
2023-03-20 23:39:58 +0500

    equal comparator

---
+++ java script/comparator.js
@@ -0,0 +1,19 @@
+var a = 2;
+var b = "2";
+// two == equal can't check the datatypes if equal then equal
+if(a == b){
+    console.log("yes");
+}
+else
+{
+    console.log("no");
+}
+
+// three === equal can check the datatypes of variable
+if(a === b){
+    console.log("yes");
+}
+else
+{
+    console.log("no");
+}
