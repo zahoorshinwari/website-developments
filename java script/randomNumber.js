ac68dafca12705586c5464036895864fdd98b06a
zahoorkhan <khanzshinwari5371@gmail.com>
2023-03-20 23:13:13 +0500

    random number generating

---
+++ java script/randomNumber.js
@@ -0,0 +1,13 @@
+var x = Math.random();
+x = x * 6;
+x = Math.floor(x) + 1;
+console.log(x);
+
+
+
+// prompt("what is your name?");
+// prompt("who is your crush?");
+
+// var loveScore = Math.random() * 100 ;
+// loveScore = Math.floor(loveScore) + 1;
+// alert("your love score is " + loveScore + "%");
