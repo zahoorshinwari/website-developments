bb7ced117fe207ecda1af05abf81de9f7fbdb816
zahoorkhan <khanzshinwari5371@gmail.com>
2023-03-22 00:03:47 +0500

    updated

---
+++ java script/array.js
@@ -0,0 +1,24 @@
+var guestList = ["zahoor", "khan", "ali", "arman", "ikram"];
+var insert = prompt("enter the name that you want to search.");
+if(insert === guestList.length){
+
+    console.log("welcome");
+}else{
+    console.log("sorry we did'nt find.");
+}
+
+
+
+
+
+
+
+// var guestList = ["zahoor", "khan", "ali", "arman", "ikram"];
+// var insert = prompt("enter the name that you want to search.");
+// // if(insert === guestList.length)
+// if(guestList.includes(insert))
+// {
+//     alert("welcome");
+// }else{
+//     alert("sorry we did'nt find.");
+// }
