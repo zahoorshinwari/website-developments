27723ec1138ca3cea4198886c6b915b57c15683c
zahoorkhan <khanzshinwari5371@gmail.com>
2023-03-22 23:15:29 +0500

    array updated

--- java script/array.js
+++ java script/array.js
@@ -1,11 +1,11 @@
-var guestList = ["zahoor", "khan", "ali", "arman", "ikram"];
-var insert = prompt("enter the name that you want to search.");
-if(insert === guestList.length){
+// var guestList = ["zahoor", "khan", "ali", "arman", "ikram"];
+// var insert = prompt("enter the name that you want to search.");
+// if(insert === guestList.length){
 
-    console.log("welcome");
-}else{
-    console.log("sorry we did'nt find.");
-}
+//     console.log("welcome");
+// }else{
+//     console.log("sorry we did'nt find.");
+// }
 
 
 
@@ -22,3 +22,34 @@ if(insert === guestList.length){
 // }else{
 //     alert("sorry we did'nt find.");
 // }
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////
+//////// fizzbuzz problem solution /////////////
+var output = [];
+var count = 1;
+
+function fizzbuzz(){
+    if(count % 3 === 0 && count % 5 === 0){
+        output.push("fizzbuzz");
+    }else if(count % 3 === 0){
+        output.push("fizz");
+    }else if(count % 5 === 0){
+        output.push("buzz");
+    }else 
+    {
+        output.push(count)
+    }
+    
+    
+    count ++;
+    // count = count + 1;
+    console.log(output);
+}
