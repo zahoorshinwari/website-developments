b86e60d99e67ecce118df326beba8b00a375b86b
zahoorkhan <khanzshinwari5371@gmail.com>
2023-03-25 01:42:15 +0500

    updated

--- java script/fibbonacci.js
+++ java script/fibbonacci.js
@@ -1,21 +1,66 @@
 
-function fibonacciGenerator(n){
-    var output = [];
-    if (n === 1){
-        output = [0];
-    }
-    else if(n === 2){
-        output = [0,1];
+// function fibonacciGenerator(n){
+//     var output = [];
+//     if (n === 1){
+//         output = [0];
+//     }
+//     else if(n === 2){
+//         output = [0,1];
+//     }
+//     else{
+//         output = [0,1];
+//         for(var i = 2; i<n; i++){
+//             output.push(output[output.length - 2] + output[output.length - 1]);
+//         }
+//     }
+
+//     return output;
+// }
+
+// output = fibonacciGenerator(5);
+// console.log(output);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function fibbonacciGenerator(n){
+    var result = [];
+    if(n === 1){
+        result = [0];
+    }else if(n === 2){
+        result = [0,1];
     }
     else{
-        output = [0,1];
+        result = [0,1];
         for(var i = 2; i<n; i++){
-            output.push(output[output.length - 2] + output[output.length - 1]);
+        result.push(result[result.length - 2] + result[result.length - 1]);
         }
     }
-
-    return output;
+    return result;
 }
 
-output = fibonacciGenerator(5);
-console.log(output);
+result = fibbonacciGenerator(3);
+console.log(result);
