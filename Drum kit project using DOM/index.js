7b412c41a200af21dc8bcaa25ae35168b3b37e99
zahoorkhan <khanzshinwari5371@gmail.com>
2023-03-30 02:13:37 +0500

    added the eventListener into the buttons when it become clicked

---
+++ Drum kit project using DOM/index.js
@@ -0,0 +1,25 @@
+// it is also same as below but it has some problem in the html file there will be also more 
+// and more button then it will apply on all of these.
+
+// var numberOfDrums = document.querySelectorAll(".drum").length;
+// for(var i = 0; i<document.querySelectorAll("button").length; i++) {
+//         document.querySelectorAll("button")[i].addEventListener("click" , function () {
+//         alert("I got clicked.");
+//     });
+// }
+
+
+
+
+
+//////////////////////////////////////////////////////////////////
+var numberOfDrums = document.querySelectorAll(".drum").length;
+for(var i = 0; i<numberOfDrums; i++) {
+        document.querySelectorAll(".drum")[i].addEventListener("click" , function () {
+        alert("I got clicked.");
+    });
+    
+}
+
+
+
