fea5b8a024fec1209923f73d7a8f864e0959fe78
zahoorkhan <khanzshinwari5371@gmail.com>
2023-04-01 22:53:18 +0500

    adding function inside the objects

--- java script/objects.js
+++ java script/objects.js
@@ -9,17 +9,34 @@
 
 
 // it is constructor function
+// function BellBoy (name, age, experience, language){
+//     this.name = name;
+//     this.age = age;
+//     this.experience = experience;
+//     this.language = language;
+// }
+
+
+// var bellBoy1 = new BellBoy("ali", 23, 3, ["pashto", "urdu"]);
+// var bellBoy2 = new BellBoy("ikram", 27, 6, ["arabi", "english"]);
+// var bellBoy3 = new BellBoy("umar", 26, 5, ["spanish", "french"]);
+// console.log(bellBoy1);
+// console.log(bellBoy2);
+// console.log(bellBoy3);
+
+
+
+// adding the function to the objects
 function BellBoy (name, age, experience, language){
     this.name = name;
     this.age = age;
     this.experience = experience;
     this.language = language;
+    this.clean = function(){
+        console.log("cleaning the progress....");
+        // alert("cleaning in progress....");
+    }
 }
 
-
 var bellBoy1 = new BellBoy("ali", 23, 3, ["pashto", "urdu"]);
-var bellBoy2 = new BellBoy("ikram", 27, 6, ["arabi", "english"]);
-var bellBoy3 = new BellBoy("umar", 26, 5, ["spanish", "french"]);
-console.log(bellBoy1);
-console.log(bellBoy2);
-console.log(bellBoy3);
+console.log(bellBoy1.clean());
