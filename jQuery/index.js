a5b2c45dd0adc02759662a4e1b262ab746ef01ad
zahoorkhan <khanzshinwari5371@gmail.com>
2023-04-03 23:32:59 +0500

    jQuery used and (cdn link of jQuery)

---
+++ jQuery/index.js
@@ -0,0 +1,13 @@
+
+
+// using ready method and function to run jQuery
+// $(document).ready(function(){
+//     $("h1").css("color", "green");
+// });
+
+
+// using dollar sign to apply jquery
+$("h1").css("font-size", "5rem");
+
+// writing jquery to apply jquery on elements or tags
+jQuery("h1").css("color", "blue");
