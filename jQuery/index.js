1776e03a7d5ac212d554b80cfbc92a41807d377f
zahoorkhan <khanzshinwari5371@gmail.com>
2023-04-04 00:25:26 +0500

    adding the styles to the element using jquery

--- jQuery/index.js
+++ jQuery/index.js
@@ -7,7 +7,15 @@
 
 
 // using dollar sign to apply jquery
-$("h1").css("font-size", "5rem");
+// $("h1").css("font-size", "5rem");
 
 // writing jquery to apply jquery on elements or tags
-jQuery("h1").css("color", "blue");
+// jQuery("h1").css("color", "blue");
+
+
+
+// adding class
+$("h1").addClass("title-heading margin"); 
+
+// removing class
+// $("h1").removeClass("title-heading");
