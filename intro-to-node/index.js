394a098b9b667d702f9dddef1accb530ab4886a1
zahoorkhan <khanzshinwari5371@gmail.com>
2023-05-21 01:21:33 +0500

    node commands

---
+++ intro-to-node/index.js
@@ -0,0 +1,232 @@
+ console.log("hello world");
+
+
+
+
+
+
+
+
+
+// node commands and read Evalution Print Loop
+
+
+// 92333@zahoorshinwari66 MINGW64 ~
+// $ s
+// bash: s: command not found
+
+// 92333@zahoorshinwari66 MINGW64 ~
+// $ ls
+//  -1.14-windows.xml        ntuser.dat.LOG2
+// '3D Objects'/             NTUSER.DAT{53b39e88-18c4-11ea-a811-000d3aa4692b}.TM.blf
+//  AndroidStudioProjects/   NTUSER.DAT{53b39e88-18c4-11ea-a811-000d3aa4692b}.TMContainer00000000000000000001.regtrans-ms
+//  AppData/                 NTUSER.DAT{53b39e88-18c4-11ea-a811-000d3aa4692b}.TMContainer00000000000000000002.regtrans-ms
+// 'Application Data'@       ntuser.ini
+//  Contacts/                OneDrive/
+//  Cookies@                 pic/
+//  Desktop/                 Pictures/
+//  Documents/               PrintHood@
+//  Downloads/               Recent@
+//  Favorites/               saad.py
+//  IntelGraphicsProfiles/  'Saved Games'/
+//  Links/                   Searches/
+// 'Local Settings'@         SendTo@
+//  MediaGet2/              'Start Menu'@
+//  Music/                   Templates@
+// 'My Documents'@           Untitled-1.txt
+//  NetHood@                 Videos/
+//  NTUSER.DAT              'VirtualBox VMs'/
+//  ntuser.dat.LOG1
+
+// 92333@zahoorshinwari66 MINGW64 ~
+// $ pwd
+// /c/Users/92333
+
+// 92333@zahoorshinwari66 MINGW64 ~
+// $ cd d
+// bash: cd: d: No such file or directory
+
+// 92333@zahoorshinwari66 MINGW64 ~
+// $ cd /d
+
+// 92333@zahoorshinwari66 MINGW64 /d
+// $ ls
+// '$RECYCLE.BIN'/                       java/                                                'resourses of webdevelopment'/
+//  3JS/                                'java script'/                                        'simple javascript projects'/ 
+// 'all of my own project on the web'/  'javascript projects'/                                'System Volume Information'/  
+//  CV/                                 'Legand UI'/                                           Temp/
+// 'E-commerce page'/                    Mobile-App-UI/                                        tmp/
+// 'for practice'/                      'Program Files'/                                      'udemy web course'/    
+// 'graphics designing'/                 PyOpenGL_accelerate-3.1.6-cp310-cp310-win_amd64.whl  'university works'/    
+//  GUI_projects/                        PyOpenGL-3.1.6-cp310-cp310-win_amd64.whl             'website developments'/
+
+// 92333@zahoorshinwari66 MINGW64 /d
+// $ cd website\ developments/
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments (main)
+// $ ls
+//  Bootstrap/       cssapple/    'DOM(document object model)'/   jQuery/                project1/
+// 'COMMAND LINE'/   CV/          'Drum kit project using DOM'/   playground/           'Simon Game Challenge Starting Files'/
+//  CSS_MY_Site/    'Dice game'/  'java script'/                  playground-html-css/
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments (main)
+// $ touch "intro-to-node"
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments (main)
+// $ ls
+//  Bootstrap/       cssapple/    'DOM(document object model)'/  'java script'/   playground-html-css/
+// 'COMMAND LINE'/   CV/          'Drum kit project using DOM'/   jQuery/         project1/
+//  CSS_MY_Site/    'Dice game'/   intro-to-node                  playground/    'Simon Game Challenge Starting Files'/
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments (main)
+// $ cd intro-to-node 
+// bash: cd: intro-to-node: Not a directory
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments (main)
+// $ cd "intro-to-node" 
+// bash: cd: intro-to-node: Not a directory
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments (main)
+// $ cd intro-to-node 
+// bash: cd: intro-to-node: Not a directory
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments (main)
+// $ rm intro-to-node 
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments (main)
+// $ ls
+//  Bootstrap/       cssapple/    'DOM(document object model)'/   jQuery/                project1/
+// 'COMMAND LINE'/   CV/          'Drum kit project using DOM'/   playground/           'Simon Game Challenge Starting Files'/    
+//  CSS_MY_Site/    'Dice game'/  'java script'/                  playground-html-css/
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments (main)
+// $ mkdir "intro-to-node"
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments (main)
+// $ ls
+//  Bootstrap/       cssapple/    'DOM(document object model)'/  'java script'/   playground-html-css/
+// 'COMMAND LINE'/   CV/          'Drum kit project using DOM'/   jQuery/         project1/
+//  CSS_MY_Site/    'Dice game'/   intro-to-node/                 playground/    'Simon Game Challenge Starting Files'/
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments (main)
+// $ cd intro-to-node/
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ ls
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ touch index.js
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ ls
+// index.js
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ cat index.js 
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ start "vs-code" index.js 
+// The system cannot find the file vs-code.
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ ls
+// index.js
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ cat index.js 
+// console.log("hello world");
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ node index.js 
+// hello world
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ start "Visual Studio Code" index.js 
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ ks
+// bash: ks: command not found
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ ls
+// index.js
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ code .
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ node index.js 
+// hello world
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ ls
+// index.js
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ node
+// Welcome to Node.js v18.16.0.
+// Type ".help" for more information.
+// > console.log("how are you");
+// how are you
+// undefined
+// > clg
+// Uncaught ReferenceError: clg is not defined
+// > consoe.log("where are you");
+// Uncaught ReferenceError: consoe is not defined
+// > console.log("where are you");
+// where are you
+// undefined
+// > ls
+// Uncaught ReferenceError: ls is not defined
+// > 5+10
+// 15
+// > "zahoor" + "khan"
+// 'zahoorkhan'
+// > con.
+// ...
+// ...
+// ...
+// ... cd ..
+// cd ..
+//     ^
+
+// Uncaught SyntaxError: Unexpected token '.'
+// > ls
+// Uncaught ReferenceError: ls is not defined
+// > console.log("wow");
+// wow
+// undefined
+// > con
+// const        continue
+
+// console      constants
+
+// constructor
+
+// > console.
+// console.__proto__             console.constructor           console.hasOwnProperty        console.isPrototypeOf
+// console.propertyIsEnumerable  console.toLocaleString        console.toString              console.valueOf
+
+// console.Console               console._ignoreErrors         console._stderr               console._stderrErrorHandler
+// console._stdout               console._stdoutErrorHandler   console._times                console.assert
+// console.clear                 console.context               console.count                 console.countReset
+// console.debug                 console.dir                   console.dirxml                console.error
+// console.group                 console.groupCollapsed        console.groupEnd              console.info
+// console.log                   console.profile               console.profileEnd            console.table
+// console.time                  console.timeEnd               console.timeLog               console.timeStamp
+// console.trace                 console.warn
+
+// > .exit
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ ^C
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ node
+// Welcome to Node.js v18.16.0.
+// Type ".help" for more information.
+// >
+// (To exit, press Ctrl+C again or Ctrl+D or type .exit)
+// >
+
+// 92333@zahoorshinwari66 MINGW64 /d/website developments/intro-to-node (main)
+// $ clear
