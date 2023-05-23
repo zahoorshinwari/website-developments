2a1b264aa1429992fe607d9b762e99f696745186
zahoorkhan <khanzshinwari5371@gmail.com>
2023-05-23 23:48:37 +0500

    updated the routes

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -4,12 +4,35 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(request , response){
-    //console.log(response);
-    //response.send("hello world");
-    //response.send("<p>here we can creating the webpage that we sending data through by using node js and express js and that can be seen on the webpage</p>");
 
-    response.send("https://zahoorshinwari.github.io/Simon-Game/");
+// this is first route that we create
+app.get("/", function(request , response){
+    //  console.log(response);
+    //  response.send("hello world");
+    //  response.send("<p>here we can creating the webpage that we sending data through by using node js and express js and that can be seen on the webpage</p>");
+    //  response.send("https://zahoorshinwari.github.io/Simon-Game/");
+    //  response.send("<p>hi how are you. i am learning now the web development this is the express server data.</p>");
+
+    response.send("hello world");
+});
+
+
+
+// this is second route
+app.get("/contact" , function(request , response){
+    response.send("contact me at: khanzshinwari5371@gmail.com");
+});
+
+
+// this is third route
+app.get("/about" , function(request , response){
+    response.send("I'm a web developer. I will design the website for you that you want. I'll be available 24/7 just contact me.");
+});
+
+
+// this is fourth route
+app.get("/hobbies" , function(request , response){
+    response.send("<ul><li>reading books </li> <li>football</li> <li>coding</li> </ul> ");
 });
 
 app.listen(3000 , function(){
