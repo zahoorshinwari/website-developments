850880bb4feee140bac84fb7525c55ecd91030e9
zahoorkhan <khanzshinwari5371@gmail.com>
2023-06-05 00:23:01 +0500

    updated

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -6,12 +6,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+
+ 
 const app = express();
-
-
-
-
 var items = ["Buy Food" , "Cook Food" , "Eat Food"];
+var workItems = [];
 
 // it is must to write here 
 // if not declare then our app can't work
@@ -20,6 +19,8 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+// to add the static files like images and css
+app.use(express.static("public") );
 
 app.get("/" , function (req , res)  {
     
@@ -35,7 +36,8 @@ app.get("/" , function (req , res)  {
 
         // use.rendor can use the view engine
     // it can load the list.ejs file to view engine 
-    res.render("list" , {kindOfDay: day , newListItems: items});
+    // newListItems are the input that are comes from the form input
+    res.render("list" , {listTitle: day , newListItems: items});
 
 })
 
@@ -44,14 +46,30 @@ app.get("/" , function (req , res)  {
 // which are comes from list.ejs file
 // and then back send to the browser list item
     app.post("/" , function(req , res){
+        
         var item = req.body.newItem;
         
-        items.push(item);
-
-        res.redirect("/");
+        if(req.body.list === "work"){
+            workItems.push(item);
+            res.redirect("/work");
+        }
+        else {
+            items.push(item);
+            res.redirect("/");
+        } 
+        
+        
     })
 
 
+
+    // it is for the /work page
+    app.get("/work" , function(req, res){
+        res.render("list" , {listTitle: "work List", newListItems: workItems});
+    })
+
+
+    
 
 app.listen(3000, ()=> console.log("server started on port 3000"));
 
